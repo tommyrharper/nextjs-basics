0f888546f14fb983f4853a2970a706025b3dd697
tommyrharper <thomasrobertharper@gmail.com>
2021-04-30 03:00:45 +0100

    Create car [id].js

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -6,55 +6,46 @@ export default function Car({ car }) {
 
     const router = useRouter()
     const { id } = router.query
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>{car.color} {car.id}</title>
-            </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    {id}
-                </h1>
-
-                <img src={car.image} width="300px" />
-
-            </main>
-        </div>
-    )
+    return (<>
+    <Head>
+      <title>{car.color} {car.id}</title>
+    </Head>
+        <h1>Hi {id}</h1>
+        <img src={car.image} />
+    </>);
 }
 
-
 export async function getServerSideProps({ params }) {
-    const req = await fetch(`http://localhost:3000/${params.id}.json`);
-    const data = await req.json();
 
-    return {
-        props: { car: data },
-    }
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  }
 }
 
 // export async function getStaticProps({ params }) {
 
-//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
-//     const data = await req.json();
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
 
-//     return {
-//         props: { car: data },
-//     }
+//   return {
+//     props: { car: data },
+//   }
 // }
 
 // export async function getStaticPaths() {
 
-//     const req = await fetch('http://localhost:3000/cars.json');
-//     const data = await req.json();
+//   const req = await fetch(`http://localhost:3000/cars.json`);
+//   const data = await req.json();
 
-//     const paths = data.map(car => {
-//         return { params: { id: car } }
-//     })
+//   const paths = data.map(car => {
+//     return { params: { id: car } }
+//   })
 
-//     return {
-//         paths,
-//         fallback: false
-//     };
+//   return {
+//     paths,
+//     fallback: false
+//   }
 // }
